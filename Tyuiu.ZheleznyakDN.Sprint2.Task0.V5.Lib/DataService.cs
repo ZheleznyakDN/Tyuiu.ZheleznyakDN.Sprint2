71bd224be536d5c2c4f249f4924c0471ebc52cb4
Железняк Дамир <Tyuiu.ZheleznyakDN@outlook.com>
2025-11-18 22:47:49 +0500

    Task0

--- Tyuiu.ZheleznyakDN.Sprint2.Task0.V5.Lib/DataService.cs
+++ Tyuiu.ZheleznyakDN.Sprint2.Task0.V5.Lib/DataService.cs
@@ -5,7 +5,7 @@ namespace Tyuiu.ZheleznyakDN.Sprint2.Task0.V5.Lib
     {
         public bool[] GetCompareOperations(int x, int y)
         {
-            bool[] res = new bool[5];
+            bool[] res = new bool[6];
             res[0] = x == y;
             res[1] = x != y;
             res[2] = x < y;
