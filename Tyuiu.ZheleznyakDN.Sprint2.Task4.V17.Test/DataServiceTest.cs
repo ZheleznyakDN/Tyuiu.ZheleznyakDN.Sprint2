d119abb988643f58cb2cfb5821b5c39a0c57459a
Железняк Дамир <Tyuiu.ZheleznyakDN@outlook.com>
2025-11-22 22:26:13 +0500

    Task4

---
+++ Tyuiu.ZheleznyakDN.Sprint2.Task4.V17.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Tyuiu.ZheleznyakDN.Sprint2.Task4.V17.Lib;
+namespace Tyuiu.ZheleznyakDN.Sprint2.Task4.V17.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            double x = 10;
+            double y = 5;
+            double result = ds.Calculate(x, y);
+            double expected = 5.12;
+
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
---
+++ Tyuiu.ZheleznyakDN.Sprint2.Task4.V17/Program.cs
@@ -0,0 +1,42 @@
+﻿using Tyuiu.ZheleznyakDN.Sprint2.Task4.V17.Lib;
+namespace Tyuiu.ZheleznyakDN.Sprint2.Task4.V17
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #2 | Выполнил: Железняк Д.Н. | ПКТб-25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Тема: Тернарный оператор                                                *");
+            Console.WriteLine("* Задание #4                                                              *");
+            Console.WriteLine("* Вариант #17                                                             *");
+            Console.WriteLine("* Выполнил: Железняк Дамир Николаевич | ПКТб-25-1                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая вычисляет требуемое значение с              *");
+            Console.WriteLine("* использованием тернарного оператора, где пользователь вводит значение   *");
+            Console.WriteLine("* переменных x,y с клавиатуры. Ответ округлить до 3 знаков после запятой. *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ :                                                       *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.Write("Введите значение X: ");
+            double x = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Введите значение Y: ");
+            double y = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ :                                                             *");
+            Console.WriteLine("***************************************************************************");
+
+            double result = ds.Calculate(x, y);
+            Console.WriteLine($"Результат вычисления: {result}");
+
+            Console.ReadKey();
+        }
+    }
+}
